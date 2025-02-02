1de8ce52440edb342c7e6a59259140e63942c18d
Nagico <nagico@qq.com>
2025-02-02 23:59:59 +0800

    feat: hide the secret config

--- config/config.py
+++ config/config.py
@@ -1,58 +1,95 @@
-# ------------------GPT info--------------------------
-# model list
-model_quester_anster = "text-davinci-003"
-model_gpt_35_turbo = "gpt-3.5-turbo"
-model_gpt_4o = "gpt-4o"
-model_programming_translate = "code-davinci-002"
+from pathlib import Path
 
-# api 接口封装类
+from dotenv import dotenv_values
+
+ROOT = Path(__file__).parent.parent
+ENV_PATH = ROOT / "config" / ".env"
+
+dot_config = dotenv_values(ENV_PATH)
+
+# ------------------Gitlab info--------------------------
+gitlab_server_url = dot_config.get("GITLAB_SERVER", "https://gitlab.com")
+gitlab_private_token = dot_config.get("GITLAB_PERSONAL_ACCESS_TOKEN", "")
+gitlab_webhook_verify_token = dot_config.get("GITLAB_WEBHOOK_VERIFY_TOKEN", None)
+
+# Gitlab modifies the maximum number of files
+maximum_files = 100
+
+
+# ------------------GPT info--------------------------
+# api impl
 llm_api_impl = "llm_api.llm_api_default.LLMApiDefault"
 
-# api 配置默使用认UnionLLM，参考：https://github.com/EvalsOne/UnionLLM/tree/main/docs
-# UnionLLM兼容LiteLLM，参考LiteLLM文档：https://docs.litellm.ai/docs
+# API config using UnionLLM by default, refer to: https://github.com/EvalsOne/UnionLLM/tree/main/docs
+# UnionLLM is compatible with LiteLLM, refer to LiteLLM documentation: https://docs.litellm.ai/docs
 api_config = {
-    "OPENAI_API_KEY": "your openai key",
-    "OPENAI_API_BASE": "https://api.openai.com/v1",
-    "MODEL_NAME": model_gpt_4o,
-    "PROVIDER": "openai",
+    "MODEL_NAME": dot_config.get("LLM_MODEL_NAME", "deepseek-r1:70b"),
+    "PROVIDER": "ollama",
+    "API_BASE": dot_config.get("LLM_API_BASE", None),
+    # http proxy for llm requests
+    "LLM_PROXY": dot_config.get("LLM_HTTP_PROXY", None),
+    # number of context tokens for ollama
+    "NUM_CTX": "32768",
 }
 
 # Prompt
 gpt_message = """
-         你是一位资深编程专家，gitlab的分支代码变更将以git diff 字符串的形式提供，请你帮忙review本段代码。然后你review内容的返回内容必须严格遵守下面的格式，包括标题内容。模板中的变量内容解释：变量5是代码中的优点儿 变量1是给review打分，分数区间为0~100分。 变量2 是code review发现的问题点。  变量3是具体的修改建议。变量4是你给出的修改后的代码。 必须要求：1. 以精炼的语言、严厉的语气指出存在的问题。2. 你的反馈内容必须使用严谨的markdown格式 3. 不要携带变量内容解释信息。4. 有清晰的标题结构。有清晰的标题结构。有清晰的标题结构。
+你是一位资深编程专家，gitlab的分支代码变更将以git diff形式提供，请你帮忙review本段代码。
+并且给出你的建议和评分，评分标准为1-10分，10分为最高分，总结一下这次代码变更的优缺点，给出你的建议。
+最后得出审核结论，是否通过这次代码变更。
+你review内容的返回内容必须严格遵守下面的格式，包括标题内容。
+
+必须要求：
+1. 以精炼的语言、严厉的语气指出存在的问题。
+2. 你的反馈内容必须使用严谨的markdown格式。
+3. 不要携带变量内容解释信息。
+4. 有清晰的标题结构。有清晰的标题结构。有清晰的标题结构。
+5. 用中文书写。
+6. 可以适当使用emoji表情。
+
 返回格式严格如下：
 
+## PR 审核结果
 
+### 改动内容
+1. **{改动内容1}**: 
+    - {详情}
+    - {详情}
+    ...
+2. **{改动内容2}**: 
+    - {详情}
+    - {详情}
+    ...
+...
 
-### 😀代码评分：{变量1}
+### 潜在问题点
+1. **{问题点1}**: 
+    - {详情}
+    - {详情}
+    ...
+2. **{问题点2}**: 
+    - {详情}
+    - {详情}
+    ...
+...
 
-#### ✅代码优点：
-{变量5}
+### 其他建议
+- {建议1}
+- {建议2}
+...
 
-#### 🤔问题点：
-{变量2}
+### 评分: {总分}分
+- **功能实现:** {分数}分
+    {评价}
+- **代码质量:** {分数}分
+    {评价}
+- **可维护性:** {分数}分
+    {评价}
 
-#### 🎯修改建议：
-{变量3}
+### 总结
+{总结}
 
-#### 💻修改后的代码：
-```python
-{变量4}
+### 审核结论
+{审核结论}
+"""
 
-         """
-
-# ------------------Gitlab info--------------------------
-# Gitlab url
-gitlab_server_url = "https://gitlab.com"
-
-# Gitlab private token
-gitlab_private_token = "gitlab private token"
-
-# Gitlab modifies the maximum number of files
-maximum_files = 50
-
-
-# ------------- Message notification --------------------
-# dingding notification （un necessary）
-dingding_bot_webhook = "https://oapi.dingtalk.com/robot/send?access_token=*****************************************"
-dingding_secret = "S********************************950f"
